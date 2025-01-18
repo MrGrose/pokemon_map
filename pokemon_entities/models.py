a45daacc6a9f2d01a7979f0c0a3b0f858c2e7f97
MrGrose <grachev.ro@gmail.com>
2025-01-18 20:37:19 +0300

    Update models.py
    1. Fixed required fields
    2. Fixed the path for saving images
    3. Set the field for the appropriate model
    4. Fixed the logic of getting the next evolution using feedback
    5. Fix related_names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,24 +3,17 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name="Имя покемона", max_length=200)
-    title_en = models.CharField(verbose_name="Имя покемона на английском", max_length=200)
-    title_jp = models.CharField(verbose_name="Имя покемона на японском", max_length=200)
-    picture = models.ImageField(verbose_name="Картинка", null=True, blank=True)
+    title_en = models.CharField(verbose_name="Имя покемона на английском", max_length=200, blank=True)
+    title_jp = models.CharField(verbose_name="Имя покемона на японском", max_length=200, blank=True)
+    picture = models.ImageField(verbose_name="Картинка", upload_to='madia', blank=True)
+    description = models.TextField(verbose_name="Описание", blank=True)
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name="previous_evolutions",
-        verbose_name="Предыдущая эволюция"
-    )
-    next_evolution = models.ForeignKey(
-        "self",
-        on_delete=models.SET_NULL,
-        null=True,
         related_name="next_evolutions",
-        blank=True,
-        verbose_name="Следующая эволюция",
+        verbose_name="Предыдущая эволюция"
     )
 
     def __str__(self):
@@ -30,10 +23,9 @@ class Pokemon(models.Model):
 class PokemonEntity(models.Model):
     lon = models.FloatField(verbose_name="Долгота")
     lat = models.FloatField(verbose_name="Широта")
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="pokemon", verbose_name="Покемон")
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, related_name="entities", verbose_name="Покемон")
     appeared_at = models.DateTimeField(verbose_name="Время появления", null=True, blank=True)
     disappeared_at = models.DateTimeField(verbose_name="Время исчезновения", null=True, blank=True)
-    description = models.TextField(verbose_name="Описание", blank=True)
     level = models.IntegerField(verbose_name="Уровень", null=True, blank=True)
     health = models.IntegerField(verbose_name="Здоровье", null=True, blank=True)
     attack = models.IntegerField(verbose_name="Атака", null=True, blank=True)
